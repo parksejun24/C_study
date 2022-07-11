2eeadde5eaa88fdb34ee0ef30f5b9679b90cad24
parksejun24 <sunnystpwns@gmail.com>
2022-07-12 00:09:09 +0900

    long time no see

---
+++ 2022_07_11.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int length=0;
+    int maxmin[2] = {0,};
+    scanf("%d ",&length);
+    int *num;
+    num = malloc(sizeof(int)*length);
+    for(int i = 0;i<length;i++){
+        scanf(" %d",&num[i]);
+    }
+    maxmin[1] = num[0];
+    for(int i=0;i<length;i++){
+        if(maxmin[0] <= num[i]){
+            maxmin[0] = num[i];
+        }
+        if(maxmin[1] >= num[i]){
+            maxmin[1] = num[i];
+        }
+    }
+    printf("%d %d",maxmin[1], maxmin[0]);	
+}
